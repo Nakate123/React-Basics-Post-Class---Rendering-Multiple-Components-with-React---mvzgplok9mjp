c8ba87fafe349a668fdb05c39f20ce122f59f28d
Nakate123 <94731837+Nakate123@users.noreply.github.com>
2022-06-17 15:57:59 +0530

    tt changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-const App = () => {
+export default function App() {
   return (
-    <div id="main">
-      <h1 data-ns-test attribute="project-name">
-        E-commerce website
-      </h1>
-      <h2 data-ns-test attribute="project-description">
-        containning all bks products
-      </h2>
+    <div>
+      <h1 data-ns-test="project-name">E-commerce website</h1>
+      <h2 data-ns-test="project-description">containning all bks products</h2>
     </div>
   );
-};
-
-export default App;
+}
