bf5f39c27913208af591b080f4bea1b485fa40b5
Nakate123 <94731837+Nakate123@users.noreply.github.com>
2022-06-17 15:46:51 +0530

    make changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test attribute="project-name">
+        E-commerce website
+      </h1>
+      <h2 data-ns-test attribute="project-description">
+        containning all bks products
+      </h2>
+    </div>
+  );
+};
 
 export default App;
